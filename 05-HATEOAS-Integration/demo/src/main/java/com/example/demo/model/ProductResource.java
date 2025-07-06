eed6c97491f238f579c3e7c997c96a72a7913811
Pham Van Phuc <phucpham.1803@gmail.com>
2025-07-06 18:27:45 +0700

    add new practice

---
+++ 05-HATEOAS-Integration/demo/src/main/java/com/example/demo/model/ProductResource.java
@@ -0,0 +1,53 @@
+package com.example.demo.model;
+
+import org.springframework.hateoas.RepresentationModel;
+
+public class ProductResource extends RepresentationModel<ProductResource> {
+
+    private int id;
+    private String name;
+    private String description;
+    private double price;
+    public ProductResource() {
+
+    }
+
+    public ProductResource(Product product) {
+        this.id = product.getId();
+        this.name = product.getName();
+        this.description = product.getDescription();
+        this.price = product.getPrice();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
